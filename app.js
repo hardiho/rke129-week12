a3544ddbce2f3339106c19a785276516ffd5cf4a
hardiho <126676453+hardiho@users.noreply.github.com>
2023-03-16 23:26:23 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+const addToDoButton = document.querySelector('#addTask');
+const inputField = document.querySelector('#userTaskInput');
+const toDoContainer = document.querySelector('.todos');
+
+inputField.addEventListener('click', selectInput);
+addToDoButton.addEventListener('click', addTask);
+
+function selectInput() {
+    inputField.style['boxShadow'] = '0 4px 2px -2px black';
+
+}
+
+function addTask() {
+    if(inputField.value.trim().length) {
+
+        //create a new li element
+       let newToDo = document.createElement('li');
+       //add a selector to the element
+       newToDo.classList.add('.li-styling');
+       //add content to the new li element
+       newToDo.innerText = inputField.value;
+       toDoContainer.appendChild(newToDo);
+
+       newToDo.addEventListener('click', () => {
+        newToDo.style.color = 'grey';
+        newToDo.style.textDecoration = 'line-through';
+       }
+       )
+
+       newToDo.addEventListener('dbclick', () => {
+            newToDoContainer.removeChild(newToDo);
+       }
+       )
+
+    }
+    else {
+        console.log('empty value');
+        inputField.style['boxShadow'] = '0 4px 2px -2px black';
+    }
+
+    inputField.value = '';
+
+}
